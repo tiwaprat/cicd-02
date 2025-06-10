d81cd218cb468a496588f9376183fbee54a0fc5e
Pratima <132903772+tiwaprat@users.noreply.github.com>
2025-06-10 22:38:51 +0530

    Update appStart.sh

--- appStart.sh
+++ appStart.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-nohup java -jar /var/lib/jenkins/workspace/StudentApp/target/studentapp-1.0.0.jar --server.port=9090 &
+nohup java -jar /var/lib/jenkins/workspace/StudentApp/target/studentapp-1.0.0.jar --server.port=9090 & disown
