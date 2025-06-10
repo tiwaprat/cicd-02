e015c27b66b173f2bdbf4380b0b08af72f12cdfe
Pratima <132903772+tiwaprat@users.noreply.github.com>
2025-06-11 00:08:46 +0530

    Update appStart.sh

--- appStart.sh
+++ appStart.sh
@@ -1,3 +1,12 @@
-nohup java -jar /var/lib/jenkins/workspace/StudentApp/target/studentapp-1.0.0.jar --server.port=9090 > app.log 2>&1 &
-disown
+#!/bin/sh
 
+# Kill old instance if running
+PID=$(lsof -ti:9090)
+if [ -n "$PID" ]; then
+    kill -9 $PID
+    echo "Killed old app running on port 9090"
+fi
+
+# Start new instance in background and detach
+nohup java -jar /var/lib/jenkins/workspace/StudentApp/target/studentapp-1.0.0.jar --server.port=9090 &
+disown
