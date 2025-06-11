79b99194e5506e38ce89e0c68fe1c5bed76225a0
Pratima <132903772+tiwaprat@users.noreply.github.com>
2025-06-11 09:09:13 +0530

    Update appStart.sh

--- appStart.sh
+++ appStart.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+cd "$(dirname "$0")" || exit 1
+
+# Prevent Jenkins from killing the process
+export BUILD_ID=dontKillMe
 
 # Try to get PID of process using port 9090; suppress non-zero exit with '|| true'
 PID=$(lsof -ti:9090 || true)
@@ -12,5 +16,6 @@ else
 fi
 
 # Start new instance in background and detach
-nohup java -jar target/studentapp-1.0.0.jar --server.port=9090 & disown
+nohup java -jar target/studentapp-1.0.0.jar --server.port=9090 > app.log 2>&1 &
+disown
 
